5860d11a8c81a86fc0fb640ef1221628dfee8ae1
AnteTeno <tenoranta@gmail.com>
2026-01-11 12:20:05 +0200

    HW1 completed: Succesfully implemented all requirements for HW1

--- app/src/main/java/com/example/coursework/MainActivity.kt
+++ app/src/main/java/com/example/coursework/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.coursework
 
+import android.content.res.Configuration
+import android.mtp.MtpEvent
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,11 +19,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.node.ModifierNodeElement
@@ -26,46 +40,104 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.coursework.ui.theme.CourseworkTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            CourseworkTheme() {
+                Conversation(SampleData.conversationSample)
+                }
+            }
+        }
+    }
+
+
+data class Message(val author: String, val body: String)
+
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp).padding(top = 24.dp)){
+        Image(
+            painter = painterResource(R.drawable.screenshot_2026_01_08_141216),
+            contentDescription = null,
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if(isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface
+        )
+
+        Column(modifier = Modifier.clickable{ isExpanded = !isExpanded}){
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 20.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if(isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }
     }
 }
 
-data class Message(val author: String, val body: String)
-
 @Composable
-fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)){
-        Image(
-            painter = painterResource(R.drawable.screenshot_2026_01_08_141216),
-            contentDescription = "Contact profile picture",
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-        )
-    }
-
-    Spacer(modifier = Modifier.width(8.dp))
-
-    Column{
-        Text(text = msg.author)
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(text = msg.body)
+fun Conversation(messages: List<Message>) {
+    LazyColumn() {
+        items(messages) { message ->
+            MessageCard(message)
+        }
     }
 }
-
 
 @Preview
 @Composable
-fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Lexi", "Hello sexy")
-    )
-
+fun PreviewConversation() {
+    CourseworkTheme() {
+        Conversation(SampleData.conversationSample)
+    }
 }
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    CourseworkTheme() {
+        Surface() {
+            MessageCard(
+                msg = Message("Ante", "Hello, im learning JetPack Compose!")
+            )
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/coursework/SampleData.kt
@@ -0,0 +1,75 @@
+package com.example.coursework
+
+import com.example.coursework.Message
+
+object SampleData {
+
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Ante",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Ante",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        Message(
+            "Ante",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        Message(
+            "Ante",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Ante",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        Message(
+            "Ante",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Ante",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Ante",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Ante",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Ante",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Ante",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Ante",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Lexi",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
